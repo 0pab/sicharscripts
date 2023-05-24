6275402cc5da6008e760ae5ea024fbfba1282fce
Pablo Arango <parango@kanate.local>
2023-05-24 16:30:51 -0500

    First commit

---
+++ Assets/Bowling/BowlingAlley.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlingAlley : MonoBehaviour
+{
+    public float forwardForce;
+    public float leftBarrier;
+    public float rightBarrier;
+    public float moveIncrement;
+    public Rigidbody rigidBody;
+
+
+    public void Bowl()
+    {
+        if (rigidBody != null)
+        {
+            rigidBody.AddForce(transform.forward * forwardForce, ForceMode.Impulse);
+        }
+    }
+}
